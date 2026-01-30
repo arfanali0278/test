9ffd31982ff36c1ae0ee169dc429c6cd7c9e59f6
FaziHamza <fazihamza887r@gmail.com>
2026-01-30 12:35:47 +0500

    work tree

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GetDbModule } from './database/get-db/get-db.module';
 import { AzureBlobModule } from './database/azure-blob/azure-blob.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [GetDbModule,AzureBlobModule],
+  imports: [GetDbModule,AzureBlobModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
